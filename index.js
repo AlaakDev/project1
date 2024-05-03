52e2955c9a6f9391f4d69c2b8e07205efecd8db3
Elijah Alaak <alaakelijah78@gmail.com>
2024-05-03 06:29:35 +0300

    completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const amount = document.getElementById("amount")
+const fromCurrency = document.getElementById("from-currency-select")
+const toCurrency = document.getElementById("to-currency-select")
+const convertButton = document.getElementById("convertButton")
+convertButton.addEventListener("click", convertCurrency)
+
+convertCurrency() => {
+
+}
+
+https://v6.exchangerate-api.com/v6/cc54afd12bb7c5e0b2316b7f/latest/
